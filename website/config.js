5bd4b09c559605458228de996844241c3990d6ae
abduxla <abdullahjr711@gmail.com>
2026-04-25 16:00:23 +0530

    Add PetaFinds marketing site

---
+++ website/config.js
@@ -0,0 +1,5 @@
+window.PETAFINDS_LINKS = {
+  APP_STORE_URL: "https://apps.apple.com/app/petafinds", // TODO: Replace with live App Store URL before launch.
+  PLAY_STORE_URL: "https://play.google.com/store/apps/details?id=com.petafinds.app", // TODO: Replace with live Google Play URL before launch.
+  BUSINESS_SIGNUP_URL: "#business", // TODO: Replace with live business signup URL when ready.
+};
